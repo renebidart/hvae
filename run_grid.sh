ffaa8950e00dc43a6366e14f4370dbdea9ba36e2
renebidart <renebidart@gmail.com>
2018-12-17 23:23:32 -0500

    last tests for project

--- run_grid.sh
+++ run_grid.sh
@@ -4,3 +4,5 @@ python train.py --device 0 -c configs/HVAE_pre_L32_L32_rand_size_loc_g8.json
 
 python train.py --device 0 -c configs/HVAE_L32_L32_rand_size_g8.json
 python train.py --device 0 -c configs/HVAE_L32_L32_rand_size_loc_g8.json
+
+python train.py --device 0 -c configs/HVAE_pre_L64_L32_rand_size_loc_g8_struc2_final.json
